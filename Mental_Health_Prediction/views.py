d098f00202c6c0e052dd823922ef7ba23a0bb693
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2023-11-05 11:53:31 +0530

    fixed cors error

--- Mental_Health_Prediction/views.py
+++ Mental_Health_Prediction/views.py
@@ -8,11 +8,12 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-model = load('./Savedmodels/ml-models.joblib')
+model = load('Savedmodels/ml-models.joblib')
 
 class Predict(APIView):
     def post(self, request):
         response = request.data.get('response')
-        numeric_response = [[response['age'], response['gender'], response['stress_life_changes'], response['sleep_energy'], response['mood_emotions'], response['social_support'], response['isolation'], response['self_harm_thoughts'], response['eating_habits'], response['work_school_performance']]]
+        numeric_response = [[41, 1, 0, 0, 4, 0, 0, 2, 1, 1]]
+        # numeric_response = [[response['age'], response['gender'], response['stress_life_changes'], response['sleep_energy'], response['mood_emotions'], response['social_support'], response['isolation'], response['self_harm_thoughts'], response['eating_habits'], response['work_school_performance']]]
         model_fields = np.array(numeric_response,dtype=object).reshape((1,-1))
         return Response(round((model.predict(model_fields))[0]))
--- care_compass/settings.py
+++ care_compass/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'Mental_Health_Prediction',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -54,8 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["https://loaclhost"]
 ROOT_URLCONF = 'care_compass.urls'
 
 TEMPLATES = [
