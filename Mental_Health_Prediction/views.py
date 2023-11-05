545e350dee7608eeae03a74d5e00b64567839e09
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2023-11-05 09:44:35 +0530

    updated requirements.txt

--- Mental_Health_Prediction/views.py
+++ Mental_Health_Prediction/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import pandas
 from joblib import load
-# Create your views here.
+import numpy as np
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 model = load('./Savedmodels/ml-models.joblib')
+
+class Predict(APIView):
+    def post(self, request):
+        response = request.data.get('response')
+        numeric_response = [[response['age'], response['gender'], response['stress_life_changes'], response['sleep_energy'], response['mood_emotions'], response['social_support'], response['isolation'], response['self_harm_thoughts'], response['eating_habits'], response['work_school_performance']]]
+        model_fields = np.array(numeric_response,dtype=object).reshape((1,-1))
+        return Response(round((model.predict(model_fields))[0]))
--- care_compass/settings.py
+++ care_compass/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ife1*y(6enc0!vko!s-y-w^z$h1!0v+$zh^ndckdfadbmk#)*q'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'Mental_Health_Prediction',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +75,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'care_compass.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
